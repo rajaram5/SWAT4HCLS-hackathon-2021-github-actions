5528e73eb31cf632c872ec614a20b133b12a1771
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-01-18 16:36:11 +0100

    More rdf type files are checked and file renamed

---
+++ scripts/RDFSyntaxicValidation.py
@@ -0,0 +1,25 @@
+import glob
+import logging
+from rdflib import Graph
+import sys
+
+# set log level
+logging.basicConfig(level=logging.INFO)
+
+root_path = "../"
+
+rdf_file_extension = {".ttl":"turtle", ".nt":"nt", ".rdf":"application/rdf+xml"}
+
+for extension in rdf_file_extension.keys() :
+    files_to_check = "**/*" + extension
+    for filename in glob.iglob(root_path + files_to_check, recursive=True):
+         logging.info("Validating file " + filename)
+         try:
+             graph = Graph()
+             graph.parse(filename, format = rdf_file_extension[extension])
+         except Exception as e:
+             logging.error(e)
+             logging.error("Syntaxic error reading turtle file [" +filename+"]")
+             sys.exit(1)
+
+print("RDF files syntaxic validation is successful")
